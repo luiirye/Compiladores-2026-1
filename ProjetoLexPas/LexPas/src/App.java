7089bb037897247b8523bd6d7e1ca99b1f590f14
luiirye <borg.esluii01645@gmail.com>
2026-05-12 10:40:43 -0400

    Correções

--- ProjetoLexPas/LexPas/src/App.java
+++ ProjetoLexPas/LexPas/src/App.java
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 //import Testes.TesteBufferPrimario;
 //import Testes.TesteToken;
-=======
 import Testes.TesteBufferPrimario;
 import Testes.TesteToken;
->>>>>>> 4bcf9eaaefd26b6cc4c3377f03845fedc0836088
 import Testes.*;
 
 public class App {
