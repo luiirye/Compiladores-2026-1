62260a4e8d5151af854c270f512f3dfc31552689
Luiirye <borg.esluii01645@gmail.com>
2026-05-18 20:31:05 -0400

    Correções Classe Processamento e Main

--- ProjetoLexPas/LexPas/src/App.java
+++ ProjetoLexPas/LexPas/src/App.java
@@ -1,7 +1,13 @@
-import Testes.*;
+import Mecanismo.Processamento;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        TesteBufferSecundario.executar();
+        executar();
+    }
+
+    private static void executar(){
+        Processamento proc = new Processamento();
+        proc.prepararTabelaSimbolosPrograma();
+        proc.imprimirTabelaSimbolosPrograma();
     }
 }
--- ProjetoLexPas/LexPas/src/Mecanismo/Processamento.java
+++ ProjetoLexPas/LexPas/src/Mecanismo/Processamento.java
@@ -1,29 +1,47 @@
 package Mecanismo;
 
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import Testes.TesteBufferPrimario;
+import Testes.TesteBufferSecundario;
+
+import Dominio.Token;
 
 public class Processamento {
 
-    public Processamento() {
+    private ArrayList<Token> tabelaSimbolosPrograma;
 
+    public Processamento(){
     }
 
-    public void prepararTabelaSimbolosPrograma() {
-        
+    public void prepararTabelaSimbolosPrograma(){
         LeitorDeArquivo leitor = new LeitorDeArquivo();
-        
-        String caminho = "C:\\Temp\\exemplo.txt";
+        String caminho = "C:\\Users\\Docker\\Documents\\Estudos\\Compiladores-2026-1\\ProjetoLexPas\\LexPas\\src\\Temp\\exemplo.pas";
         //String caminho = "/home/colossus/Documents/GitHub/2026-1-CPL/ProjetoLexPas/LexPas/src/Temp/exemplo.pas";
         leitor.carregarArquivoParaLeitor(caminho);
         leitor.importarArquivoParaLeitor();
 
         BufferedReader br = leitor.getLeitor();
         BufferPrimario bfp = new BufferPrimario(br);
-        
         bfp.processarArquivoNoBufferPrimario();
         bfp.imprimirConteudoDoBufferPrimario(true);
 
-        
+        ArrayList<String> buffer = bfp.getBufferPrimario();
+        BufferSecundario bfs = new BufferSecundario(buffer);
+        bfs.processarBufferSecundario();
+        bfs.imprimirConteudoBufferSecundario(true);
+
+        this.tabelaSimbolosPrograma = bfs.getBufferSecundario();
     }
 
+    public void imprimirTabelaSimbolosPrograma(){
+        System.out.println("**************************************");
+        System.out.println("*** TABELA DE SIMBOLOS DO PROGRAMA ***");
+        System.out.printf("%-20s|%-15s\n", "LEXEMA", "TIPO IDENTIFICADOR");
+        System.out.println("**************************************");
+
+        for (Token token : this.tabelaSimbolosPrograma) {
+            System.out.printf("%-20s|%-15s\n", token.getLexema(), token.getTipo());
+        }
+    }
 }
